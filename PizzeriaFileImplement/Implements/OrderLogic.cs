bfb56c71510eb5a7298e262e3b6eaca792d5d264
Your Name <kalyadin.maksim@mail.ru>
2020-03-06 14:19:58 +0400

    lab2

--- PizzeriaFileImplement/Implements/OrderLogic.cs
+++ PizzeriaFileImplement/Implements/OrderLogic.cs
@@ -66,6 +66,7 @@ namespace PizzeriaFileImplement.Implements
         }
         public List<OrderViewModel> Read(OrderBindingModel model)
         {
+            
             return source.Orders
             .Where(rec => model == null || rec.Id == model.Id)
             .Select(rec => new OrderViewModel
@@ -74,6 +75,7 @@ namespace PizzeriaFileImplement.Implements
                 Count = rec.Count,
                 TimeCreate = rec.TimeCreate,
                 TimeImplement = rec.TimeImplement,
+                PizzaName = source.Pizzas.FirstOrDefault((r) => r.Id == rec.PizzaId).PizzaName,
                 PizzaId = rec.PizzaId,
                 Status = rec.Status,
                 Sum = rec.Sum
