e18087dcda2cca1ac849a041f6794acc05079e4d
Your Name <kalyadin.maksim@mail.ru>
2020-03-06 13:56:45 +0400

    lab3

--- PizzeriaDatabaseImplement/Implements/OrderLogic.cs
+++ PizzeriaDatabaseImplement/Implements/OrderLogic.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using PizzeriaDatabaseImplement.Models;
+using PizzeriaBusinessLogic.Interfaces;
+using PizzeriaBusinessLogic.BindingModels;
+using PizzeriaBusinessLogic.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace PizzeriaDatabaseImplement.Implements
 {
-    class OrderLogic
+    public class OrderLogic
     {
+
     }
 }
